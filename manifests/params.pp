a63461bba3d4b1d6ce88aa170006f9603e271a9f
Jonatan Meneghetti <jonatan@unimestre.com>
2015-12-16 18:16:35 -0200

    Configurado o template.

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@ class bacula::params {
   $bacula_clients_dir     = '/etc/bacula/conf.d/Clients',
   $client_conf            = '/etc/bacula/bacula-fd.conf',
   $client_conf_template   = 'bacula/bacula-fd.conf.erb',
+  $dir_client_template     = 'bacula/bacula-client.conf.erb',
   $pid_dir                = '/var/run/bacula',
   $working_dir            = '/var/lib/bacula',
 }
