5b861943644c5740f3a8a3c7485b450eff5575c0
Jonatan Meneghetti <jonatan@unimestre.com>
2015-12-18 16:40:21 -0200

    Adicionado notificação de alteração da classe quando alterada informações do cliente.

--- manifests/client.pp
+++ manifests/client.pp
@@ -91,6 +91,7 @@ class bacula::client (
       path => "$bacula_clients_dir/$::fqdn.conf",
       content => template($dir_client_template),
       tag => 'baculaclient',
+      notify => Class['bacula::reload'],
     }
 
   }
