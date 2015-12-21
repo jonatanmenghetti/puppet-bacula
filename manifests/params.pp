1cb5da13af6e4ab3f98fa99845992ad96902bca2
Jonatan Meneghetti <jonatan@unimestre.com>
2015-12-21 17:17:15 -0200

    Removida classe de parametrização.

--- manifests/params.pp
+++
@@ -1,8 +0,0 @@
-class bacula::params {
-  $bacula_clients_dir     = '/etc/bacula/conf.d/Clients',
-  $client_conf            = '/etc/bacula/bacula-fd.conf',
-  $client_conf_template   = 'bacula-fd.conf.erb',
-  $dir_client_template    = 'bacula-client.conf.erb',
-  $pid_dir                = '/var/run/bacula',
-  $working_dir            = '/var/lib/bacula',
-}
